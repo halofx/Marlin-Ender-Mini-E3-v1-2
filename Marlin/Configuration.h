a548cb9a769334f0804774c20c8208e8bd9735b1
HaloFX <scott@halofx.com>
2020-04-30 00:29:07 -0500

    Tag R2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 3 r1"
+#define CUSTOM_MACHINE_NAME "Ender 3 r2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
